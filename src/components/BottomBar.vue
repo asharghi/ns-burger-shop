7b6d3144438ad8e7b92d70f25501bac9de4b0757
asharghi <a.sharghi.mail@gmail.com>
2023-02-02 00:43:46 +0100

    Add BottomBar

---
+++ src/components/BottomBar.vue
@@ -0,0 +1,33 @@
+
+<script lang="ts" setup>
+import { defineProps, computed } from "nativescript-vue";
+import Icon from "./Icon.vue";
+
+const props = defineProps({
+  selectedIndex: Number
+});
+
+</script>
+<template>
+  <GridLayout iosOverflowSafeArea="false" rows="auto,auto,1" columns="*,*,100,*,*">
+    <Icon fontSize="25" name="home" color="#EB764A" horizontalAlignment="center" marginTop="20" />
+    <Icon fontSize="25" name="favorite_border" :outlined="true" col="1" horizontalAlignment="center" color="#CBCACE"
+      marginTop="20" />
+    <ContentView col="2" padding="20" horizontalAlignment="center">
+      <ContentView width="60" height="60" background="#EB764A" class="search-shadow" borderRadius="60">
+        <Icon fontSize="25" name="search" color="white" horizontalAlignment="center" />
+      </ContentView>
+    </ContentView>
+    <Icon fontSize="25" name="notifications" :outlined="true" col="3" horizontalAlignment="center" color="#CBCACE"
+      marginTop="20" />
+    <Icon fontSize="25" name="shopping_bag" :outlined="true" col="4" horizontalAlignment="center" color="#CBCACE"
+      marginTop="20" />
+    <ContentView marginTop="5" row="1" width="5" height="5" borderRadius="10" background="#EB764A" />
+
+  </GridLayout>
+</template>
+<style>
+.search-shadow {
+  box-shadow: 0 20px 20px rgb(255, 176, 147)
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,8 @@
 <script lang="ts" setup>
-import {
-  ref,
-  computed,
-  onMounted,
-  onUnmounted,
-  $navigateTo,
-} from "nativescript-vue";
+import { ref } from "nativescript-vue";
 import Icon from "./Icon.vue";
 import CategoryItem from "./CategoryItem.vue";
+import BottomBar from "./BottomBar.vue";
 
 const categories = ref(['All', '🍖 Meats item', '🍳', '🦐', '🍕', '🍦']);
 const selectedCategoryIndex = ref(1);
@@ -16,47 +11,43 @@ function setSelectCategoryIndex(index: number) {
   selectedCategoryIndex.value = index;
 }
 
-onMounted(() => {
-  console.log("mounted");
-});
-
-onUnmounted(() => {
-  console.log("unmounted");
-});
-
 </script>
 
 <template>
   <Frame>
     <Page actionBarHidden="true">
-      <StackLayout iosOverflowSafeArea="false">
-        <GridLayout rows="auto" columns="auto, *, auto" margin="0 20">
-          <Image width="70" src="~/assets/avatar.png" />
-          <ContentView col="2" borderRadius="40" borderWidth="2" borderColor="#F5F4F4">
-            <Icon name="menu" margin="0 20" />
-          </ContentView>
-        </GridLayout>
+      <GridLayout rows="*,auto">
+        <StackLayout iosOverflowSafeArea="false">
+          <GridLayout rows="auto" columns="auto, *, auto" margin="0 20">
+            <Image width="70" src="~/assets/avatar.png" />
+            <ContentView col="2" borderRadius="40" borderWidth="2" borderColor="#F5F4F4">
+              <Icon name="menu" margin="0 20" />
+            </ContentView>
+          </GridLayout>
 
-        <Label text="Let's find &#xa;food near you" margin="20" fontSize="30" textWrap="true" />
-        <GridLayout padding="20" borderRadius="15" background="#F4F3F1" rows="auto" columns="auto,*,auto" margin="0 20">
-          <Icon name="search" col="0" marginRight="10" />
-          <TextField hint="Search" hintColor="red" :style="{ 'placeholder-color': '#5F5E64' }" col="1" />
-          <Icon name="filter_alt" :outlined="true" marginLeft="10" col="2" />
-        </GridLayout>
-        <ScrollView marginTop="10" orientation="horizontal" :scrollBarIndicatorVisible="false">
-          <StackLayout orientation="horizontal" padding="20">
-            <CategoryItem :key="index" v-for="(categoryName, index) in categories" :name="categoryName"
-              :selected="selectedCategoryIndex === index" @tap="setSelectCategoryIndex(index)" />
-          </StackLayout>
-        </ScrollView>
-        <GridLayout rows="auto" margin="20" columns="auto,*,auto">
-          <Label text="Popular items" />
-          <Label col="2" text="View more" color="#EB764A" fontSize="12" />
-        </GridLayout>
-        <ScrollView orientation="horizontal" :scrollBarIndicatorVisible="false">
+          <Label text="Let's find &#xa;food near you" margin="20" fontSize="30" textWrap="true" />
+          <GridLayout padding="20" borderRadius="15" background="#F4F3F1" rows="auto" columns="auto,*,auto"
+            margin="0 20">
+            <Icon name="search" col="0" marginRight="10" />
+            <TextField hint="Search" hintColor="red" :style="{ 'placeholder-color': '#5F5E64' }" col="1" />
+            <Icon name="filter_alt" :outlined="true" marginLeft="10" col="2" />
+          </GridLayout>
+          <ScrollView marginTop="10" orientation="horizontal" :scrollBarIndicatorVisible="false">
+            <StackLayout orientation="horizontal" padding="20">
+              <CategoryItem :key="index" v-for="(categoryName, index) in categories" :name="categoryName"
+                :selected="selectedCategoryIndex === index" @tap="setSelectCategoryIndex(index)" />
+            </StackLayout>
+          </ScrollView>
+          <GridLayout rows="auto" margin="20" columns="auto,*,auto">
+            <Label text="Popular items" />
+            <Label col="2" text="View more" color="#EB764A" fontSize="12" />
+          </GridLayout>
+          <ScrollView orientation="horizontal" :scrollBarIndicatorVisible="false">
 
-        </ScrollView>
-      </StackLayout>
+          </ScrollView>
+        </StackLayout>
+        <BottomBar row="1" />
+      </GridLayout>
     </Page>
   </Frame>
 </template>
